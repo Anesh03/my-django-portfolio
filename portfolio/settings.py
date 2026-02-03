ed860c387a9559543319eaacb41376015d278816
Anesh Meghwar <114390926+Anesh03@users.noreply.github.com>
2026-02-04 02:43:10 +0500

    Added STATIC_ROOT setting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+# This tells Django where to put files when we run collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
